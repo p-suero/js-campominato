c81d8619f0d749832c22090e41ee8a76d9741490
Paolo <paolo.suero@gmail.com>
2020-04-27 16:25:11 +0200

    Proseguo

--- main.js
+++ main.js
@@ -7,3 +7,40 @@
 // con difficoltà 0=> si gioca con numeri che vanno da 1 a 100
 // con difficoltà 1 => si gioca con numeri che vanno da 1 a 80
 // con difficoltà 2=> si gioca con numeri che vanno da 1 a 50
+
+//creo una funzione che generi numeri random
+function genera_random (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+//creo un array vuoto che contenga i numeri random
+var mine = []
+
+//costruisco un ciclo per generare 16 numeri random
+while (mine.length < 16) {
+    var numero_generato = genera_random (1, 100);
+    mine.push(numero_generato)
+}
+
+console.log(mine);
+
+// //chiedo all'utente di inserire un numero tra 1 e 100
+// do {
+//     var numero_utente = parseInt(prompt("Inserisci un numero da 1 a 100"))
+//     if (isNaN(numero_utente)) {
+//         alert("Non hai inserito un numero ")
+//     } else if (numero_utente < 1) {
+//         alert("Hai inserito un numero minore di 1")
+//     } else if (numero_utente > 100) {
+//         alert("Hai inserito un numero maggiore di 100")
+//     }
+// } while (isNaN(numero_utente) || numero_utente < 1 || numero_utente > 100);
+//
+// console.log(numero_utente);
+//
+// var risultato = mine.includes(numero_utente)
+// console.log(risultato);
+//
+// do {
+
+} while (true);
