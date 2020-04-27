d48b4d9fc21afc2c2f2fc05baece92c2b6699da0
Paolo <paolo.suero@gmail.com>
2020-04-27 18:22:54 +0200

    Perfezionato

--- main.js
+++ main.js
@@ -24,21 +24,28 @@ while (mine.length < 16) {
 
 console.log(mine);
 
+ // In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
+// La partita termina quando il giocatore becca una mina, ossia inserisce un numero "vietato", oppure se raggiunge il numero massimo possibile di numeri consentiti, ossia ha inserito tutti i numeri possibili che non sono mine!
 var punteggio = -1;
 
 do {
     var numero_utente = parseInt(prompt("Inserisci un numero tra 1 e 100"));
-    punteggio = punteggio + 1;
+
+    if (isNaN(numero_utente)) {
+        alert("Hai inserito un valore diverso dal numero")
+    } else if (numero_utente <1 || numero_utente >100) {
+        alert("hai inserito un valore non compreso tra 1 e 100")
+    } else {
+        punteggio = punteggio + 1;
+    }
+
 } while ((isNaN(numero_utente) == false && numero_utente >= 1 && numero_utente <= 100) && (mine.includes(numero_utente)== false  && punteggio <= 84 ));
 
-if (isNaN(numero_utente)) {
-    alert("Hai inserito un valore diverso dal numero")
-} else if (numero_utente <1 || numero_utente >100) {
-    alert("hai inserito un valore non compreso tra 1 e 100")
-} else if (mine.includes(numero_utente)) {
+
+if (mine.includes(numero_utente)) {
     console.log("Hai perso. E' stata colpita una bomba");
-    console.log(punteggio);
-} else {
-    console.log("Hai raggiunto il punteggio massimo");
-    console.log("Il tuo pungeggio finale è di" + punteggio);
+    console.log("Il tuo punteggio finale è di : " + punteggio);
+    //  Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito; in altre parole, deve comunicare all'utente quante giocate ha fatto prima di perdere
+} else if (punteggio >= 84) {
+    console.log("Hai vitno. Hai raggiunto il punteggio massimo");
 }
