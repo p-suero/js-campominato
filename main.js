487973cf504690d427aedb47391105b72e9001a4
Paolo <paolo.suero@gmail.com>
2020-04-27 14:58:02 +0200

    Inizio

---
+++ main.js
@@ -0,0 +1,9 @@
+// Il computer deve generare 16 numeri casuali tra 1 e 100, che saranno le "mine".
+// In seguito deve chiedere all'utente di inserire un numero alla volta, sempre compreso tra 1 e 100, che sarà la sua giocata.
+// Se il numero è presente nella lista delle mine, la partita termina, altrimenti il gioco continua chiedendo all'utente un altro numero (continua a giocare).
+// La partita termina quando il giocatore becca una mina, ossia inserisce un numero "vietato", oppure se raggiunge il numero massimo possibile di numeri consentiti, ossia ha inserito tutti i numeri possibili che non sono mine!
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito; in altre parole, deve comunicare all'utente quante giocate ha fatto prima di perdere.
+// BONUS: all'inizio della partita, il software richiede anche un livello di difficoltà all'utente che cambia il range di numeri totali (le mine saranno sempre 16):
+// con difficoltà 0=> si gioca con numeri che vanno da 1 a 100
+// con difficoltà 1 => si gioca con numeri che vanno da 1 a 80
+// con difficoltà 2=> si gioca con numeri che vanno da 1 a 50
